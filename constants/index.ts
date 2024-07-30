089d8a5de8efe257a9be7f6728ecf2e872e310d7
devwithzain <zainsoftwear11@gmail.com>
2024-07-30 16:40:09 +0500

    modified

---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+/**
+ * An array of navigation items, each with an id, title, and href.
+ * These items are used to build the main navigation menu of the application.
+ */
+export const navigationItems = [
+   {
+      id: 1,
+      title: 'About',
+      href: '/about'
+   },
+   {
+      id: 2,
+      title: 'Features',
+      href: '/features'
+   },
+   {
+      id: 3,
+      title: 'Customers',
+      href: '/customers'
+   },
+   {
+      id: 4,
+      title: 'Updates',
+      href: '/updates'
+   },
+   {
+      id: 5,
+      title: 'Help',
+      href: '/help'
+   }
+];
---
+++ public/index.ts
@@ -0,0 +1,5 @@
+export { default as logo } from './logosaas.png';
+export { default as ArrowRight } from './arrow-right.svg';
+export { default as cog } from './cog.png';
+export { default as cylinder } from './cylinder.png';
+export { default as noodle } from './noodle.png';
